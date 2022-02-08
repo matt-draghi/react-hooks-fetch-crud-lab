f2551a47a25cfd5fd2d05cce522ce58ab9b94a1c
Matt Draghi <mtd5264@gmail.com>
2022-02-08 17:40:04 -0500

    Completed GET deliverable

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({questions}) {
+  console.log(questions)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+        {questions.map((question =>{
+          return <QuestionItem question={question}/>
+        }))}
+      </ul>
     </section>
   );
 }
